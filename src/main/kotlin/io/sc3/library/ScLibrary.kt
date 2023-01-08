935f06b71c3ecbbe5aba5ba9e2653f234b0a6983
Jonathan Coates <git@squiddev.cc>
2023-01-08 21:22:23 +0000

    feat: Use Fabric's custom ingredient API

--- src/main/kotlin/io/sc3/library/ScLibrary.kt
+++ src/main/kotlin/io/sc3/library/ScLibrary.kt
@@ -1,11 +1,20 @@
 package io.sc3.library
 
+import io.sc3.library.recipe.IngredientBrew
+import io.sc3.library.recipe.IngredientEnchanted
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
-object ScLibrary {
+object ScLibrary : ModInitializer {
   val log = LoggerFactory.getLogger("ScLibrary")!!
 
   val modId = "sc-library"
   fun ModId(value: String) = Identifier(modId, value)
+
+  override fun onInitialize() {
+    CustomIngredientSerializer.register(IngredientBrew.Serializer)
+    CustomIngredientSerializer.register(IngredientEnchanted.Serializer)
+  }
 }
