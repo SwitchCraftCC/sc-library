e951189f20c16bf4e0a504721b980f92a0d24c72
Jonathan Coates <git@squiddev.cc>
2023-01-09 18:43:48 +0000

    feat: Rework some of the recipe classes
     - Remove BetterSpecialRecipe. We will no longer use it anywhere, and
       it's a little cursed (sorry Lem!).
     - Add {Shaped,Shapeless}RecipeSpec classes, which allow reusing
       minecraft's recipe serialisation code elsewhere.

---
+++ src/main/kotlin/io/sc3/library/recipe/ShapedRecipeSpec.kt
@@ -0,0 +1,57 @@
+package io.sc3.library.recipe
+
+import com.google.gson.JsonObject
+import io.sc3.library.ScLibrary.ModId
+import net.minecraft.item.ItemStack
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.RecipeSerializer
+import net.minecraft.recipe.ShapedRecipe
+import net.minecraft.recipe.book.CraftingRecipeCategory
+import net.minecraft.util.collection.DefaultedList
+
+/**
+ * The underlying structure of a [ShapedRecipe]-esque recipe, useful for writing serialisation and deserialization
+ * code.
+ */
+class ShapedRecipeSpec private constructor(
+  val group: String,
+  val category: CraftingRecipeCategory,
+  val width: Int,
+  val height: Int,
+  val ingredients: DefaultedList<Ingredient>,
+  val output: ItemStack,
+) {
+  fun write(buf: PacketByteBuf) {
+    buf.writeString(group)
+    buf.writeEnumConstant(category)
+
+    buf.writeVarInt(width)
+    buf.writeVarInt(height)
+    for (ingredient in ingredients) ingredient.write(buf)
+
+    buf.writeItemStack(output)
+  }
+
+  companion object {
+    fun ofRecipe(recipe: ShapedRecipe) = ShapedRecipeSpec(
+      recipe.group, recipe.category, recipe.width, recipe.height, recipe.ingredients, recipe.output
+    )
+
+    fun ofJson(json: JsonObject): ShapedRecipeSpec = ofRecipe(RecipeSerializer.SHAPED.read(ModId("ignore"), json))
+
+    fun ofPacket(buf: PacketByteBuf): ShapedRecipeSpec {
+      val group = buf.readString()
+      val category = buf.readEnumConstant(CraftingRecipeCategory::class.java)
+
+      val width = buf.readVarInt()
+      val height = buf.readVarInt()
+      val ingredients = DefaultedList.ofSize(width * height, Ingredient.EMPTY)
+      for (i in ingredients.indices) ingredients[i] = Ingredient.fromPacket(buf)
+
+      val output = buf.readItemStack()
+
+      return ShapedRecipeSpec(group, category, width, height, ingredients, output)
+    }
+  }
+}
---
+++ src/main/kotlin/io/sc3/library/recipe/ShapelessRecipeSpec.kt
@@ -0,0 +1,81 @@
+package io.sc3.library.recipe
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.mojang.brigadier.exceptions.CommandSyntaxException
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.StringNbtReader
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.ShapedRecipe
+import net.minecraft.recipe.ShapelessRecipe
+import net.minecraft.recipe.book.CraftingRecipeCategory
+import net.minecraft.util.JsonHelper
+import net.minecraft.util.collection.DefaultedList
+
+/**
+ * The underlying structure of a [ShapelessRecipe]-esque recipe, useful for writing serialisation and deserialization
+ * code.
+ */
+class ShapelessRecipeSpec private constructor(
+  val group: String,
+  val category: CraftingRecipeCategory,
+  val output: ItemStack,
+  val input: DefaultedList<Ingredient>
+) {
+  fun write(buf: PacketByteBuf) {
+    buf.writeString(group)
+    buf.writeEnumConstant(category)
+    buf.writeVarInt(input.size)
+    for (ingredient in input) ingredient.write(buf)
+    buf.writeItemStack(output)
+  }
+
+  companion object {
+    private val GSON = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
+
+    fun ofRecipe(recipe: ShapelessRecipe) = ShapelessRecipeSpec(
+      recipe.group, recipe.category, recipe.output, recipe.ingredients
+    )
+
+    fun ofJson(json: JsonObject): ShapelessRecipeSpec {
+      val group = JsonHelper.getString(json, "group", "") ?: ""
+      val category = CraftingRecipeCategory.CODEC.byId(
+        JsonHelper.getString(json, "category", null),
+        CraftingRecipeCategory.MISC
+      )
+
+      val inputs = DefaultedList.of<Ingredient>()
+      for (input in JsonHelper.getArray(json, "ingredients")) {
+        val ingredient = Ingredient.fromJson(input)
+        if (!ingredient.isEmpty) inputs.add(ingredient)
+      }
+
+      val outputObject = JsonHelper.getObject(json, "result")
+      val output = ShapedRecipe.outputFromJson(outputObject)
+
+      outputObject.get("nbt")?.let {
+        try {
+          val nbtJson = if (it.isJsonObject) GSON.toJson(it) else JsonHelper.asString(it, "nbt")
+          output.nbt = StringNbtReader.parse(nbtJson)
+        } catch (e: CommandSyntaxException) {
+          throw RuntimeException("Invalid NBT entry: ", e)
+        }
+      }
+
+      return ShapelessRecipeSpec(group, category, output, inputs)
+    }
+
+    fun ofPacket(buf: PacketByteBuf): ShapelessRecipeSpec {
+      val group = buf.readString()
+      val category = buf.readEnumConstant(CraftingRecipeCategory::class.java)
+
+      val size = buf.readVarInt()
+      val input = DefaultedList.ofSize(size, Ingredient.EMPTY)
+      for (i in input.indices) input[i] = Ingredient.fromPacket(buf)
+
+      val output = buf.readItemStack()
+      return ShapelessRecipeSpec(group, category, output, input)
+    }
+  }
+}
